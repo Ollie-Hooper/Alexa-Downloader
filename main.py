05bc6b30c23d0a6bdfae590d1c218a892e13868b
Ollie Hooper <35010178+Ollie-Hooper@users.noreply.github.com>
2022-03-22 15:26:01 +0000

    Creates dataframe with various data points from the history request

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pandas as pd
 import requests
 import time
 
+
 # import urllib3.exceptions
 
 
@@ -17,23 +18,37 @@ def main():
 
 
 def get_history(url):
-    history = pd.DataFrame(["timestamp", "transcript", "utterance_id", "device", "person", "utterance_type", "intent"])
+    history = pd.DataFrame(columns=["timestamp", "transcript", "utterance_id", "device", "person", "utterance_type", "intent"])
     params = get_params()
     headers = get_headers()
     done = False
     while not done:
         r = handle_request(url, params, headers)
         d = r.json()
-        records = [r['voiceHistoryRecordItems'][0] for r in d['customerHistoryRecords'] if
-                   r['voiceHistoryRecordItems'][0]['recordItemType'] == 'CUSTOMER_TRANSCRIPT']
         if not d['encodedRequestToken']:
             done = True
         else:
             params['previousRequestToken'] = d['encodedRequestToken']
-        timestamps = []
-        utterance_ids = [r['utteranceId'] for r in records]
-        transcripts = [r['transcriptText'] for r in records]
-    return history
+        records_df = create_records_df(d)
+        history = pd.concat([history, records_df])
+    return history.set_index("timestamp")
+
+
+def create_records_df(d):
+    records = [r['voiceHistoryRecordItems'][0] for r in d['customerHistoryRecords'] if
+               r['voiceHistoryRecordItems'][0]['recordItemType'] == 'CUSTOMER_TRANSCRIPT']
+    return pd.DataFrame({
+        "timestamp": [r['timestamp'] for r in records],
+        "transcript": [r['transcriptText'] for r in records],
+        "utterance_id": [r['utteranceId'] for r in records],
+        "device": [r['device']['deviceName'] for r in d['customerHistoryRecords'] if
+                   r['voiceHistoryRecordItems'][0]['recordItemType'] == 'CUSTOMER_TRANSCRIPT'],
+        "person": [r['personsInfo'][0]['personFirstName'] if r['personsInfo'] else '' for r in records],
+        "utterance_type": [r['utteranceType'] for r in d['customerHistoryRecords'] if
+                           r['voiceHistoryRecordItems'][0]['recordItemType'] == 'CUSTOMER_TRANSCRIPT'],
+        "intent": [r['intent'] for r in d['customerHistoryRecords'] if
+                   r['voiceHistoryRecordItems'][0]['recordItemType'] == 'CUSTOMER_TRANSCRIPT'],
+    })
 
 
 def download_audio(utterances, url, fp='/audio'):
